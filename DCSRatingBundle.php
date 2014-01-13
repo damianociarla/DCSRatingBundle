778675c587a80d77d556656fe4f750da8889be74
Damiano Ciarla <damiano.ciarla@gmail.com>
2014-01-13 18:00:25 +0100

    Removed twig form pass

--- DCSRatingBundle.php
+++ DCSRatingBundle.php
@@ -2,16 +2,9 @@
 
 namespace DCS\RatingBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use DCS\RatingBundle\DependencyInjection\Compiler\TwigFormPass;
 
 class DCSRatingBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
 
-        $container->addCompilerPass(new TwigFormPass());
-    }
 }
